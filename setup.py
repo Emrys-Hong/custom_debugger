a9603b49f162fc9b62adc9064dcd200fb6368877
Emrys-Hong <hongpengfei.emrys@gmail.com>
2024-10-10 16:55:51 +0800

    Update

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='emrys_utils',
-    version='0.1',
+    name='custom_debugger',
+    version='0.1.0',
     packages=find_packages(),
+    install_requires=[
+        'ipdb'
+    ],
+    author='Emrys-hong',
+    author_email='hongpengfei.emrys@gmail.com',
+    description='debugger work for both torch and normal use',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
