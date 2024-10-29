a426cf378e0a1af539f4eace0c25d69168569496
Emrys-Hong <hongpengfei.emrys@gmail.com>
2024-10-29 16:21:23 +0800

    Update

--- custom_debugger/debugger.py
+++ custom_debugger/debugger.py
@@ -1,50 +1,48 @@
+import pdb
 import sys
-from IPython.core.debugger import Pdb
-import ipdb
 
-class _IPdb(Pdb):
+import torch.distributed as dist
+
+
+class _DistributedPdb(pdb.Pdb):
     """
-    Supports using IPDB from inside a multiprocessing child process.
+    Supports using PDB from inside a multiprocessing child process.
     Usage:
-    _IPdb().set_trace()
+    _DistributedPdb().set_trace()
     """
+
     def interaction(self, *args, **kwargs):
         _stdin = sys.stdin
         try:
-            # Redirect stdin to ensure it works in child processes
-            sys.stdin = open('/dev/stdin')
-            super().interaction(*args, **kwargs)
+            sys.stdin = open("/dev/stdin")
+            pdb.Pdb.interaction(self, *args, **kwargs)
         finally:
             sys.stdin = _stdin
 
+
 def breakpoint(rank: int = 0):
     """
-    Set a breakpoint using IPDB, but only on a single rank in a distributed environment.
-    All other ranks will wait for the breakpoint to be released before continuing.
-    In a normal environment, it sets a breakpoint directly.
-
+    Set a breakpoint, but only on a single rank.  All other ranks will wait for you to be
+    done with the breakpoint before continuing.
     Args:
-        rank (int): Which rank to break on. Default: 0
+        rank (int): Which rank to break on.  Default: ``0``
     """
     try:
-        import torch.distributed as dist
         is_dist = dist.is_initialized()
         current_rank = dist.get_rank()
-    except (ImportError, RuntimeError):
-        # If torch.distributed is not available or not initialized, default to non-distributed
+    except:
         is_dist = False
         current_rank = 0
 
     if is_dist:
         if current_rank == rank:
-            pdb_instance = _IPdb()
-            pdb_instance.message(
+            pdb = _DistributedPdb()
+            pdb.message(
                 "\n!!! ATTENTION !!!\n\n"
-                f"Type 'up' to get to the frame that called breakpoint(rank={rank})\n"
+                f"Type 'up' to get to the frame that called dist.breakpoint(rank={rank})\n"
             )
-            pdb_instance.set_trace()
-        # Synchronize all processes; others will wait here
+            pdb.set_trace()
         dist.barrier()
     else:
-        # In a normal environment, just set a breakpoint directly
+        import ipdb
         ipdb.set_trace()
